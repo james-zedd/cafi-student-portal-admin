1a5811115eef846c14af298f210246cdb132d7ed
James Zedd <james.zedd@gmail.com>
2026-07-19 21:18:12 -0400

    Update background image

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import loginBg from "@/public/images/login-bg.jpg";
+import loginBg from "@/public/images/admin_bg_1.png";
 
 export default function AuthLayout({
   children,
@@ -15,7 +15,7 @@ export default function AuthLayout({
         priority
         className="object-cover"
       />
-      <div className="absolute inset-0 bg-black/10" />
+      <div className="absolute inset-0" />
       <div className="relative flex w-full min-w-[200px] max-w-sm">
         {children}
       </div>
